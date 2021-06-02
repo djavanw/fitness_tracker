2f8acc8db53e0f2b70eb19939b1798391f577d2f
Javan <71285290+djavanw@users.noreply.github.com>
2021-06-02 01:19:22 -0600

    Setting up server.js file. Routes needed.

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3070
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fitness_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+// Make the routes
+
+app.listen(PORT, () => {
+    console.log(`The server is UP and running on port number ${PORT}`)
+});

e3394b96d21da76b8ea563ebf3d9129d120b9c86
Javan <71285290+djavanw@users.noreply.github.com>
2021-06-02 22:06:07 -0600

    Added routes in server.js file. Added models folder.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const apiRoute = require("./routers/apiRoute");
+const viewRoute = require("./routers/viewRoute");
+
+// Logger middleware
+const morgan = require("morgan");
+
 const PORT = process.env.PORT || 3070
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,6 +25,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fitness_d
 });
 
 // Make the routes
+app.use(apiRoute);
+app.use(viewRoute);
 
 app.listen(PORT, () => {
     console.log(`The server is UP and running on port number ${PORT}`)

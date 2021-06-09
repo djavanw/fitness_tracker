13fd709c4be844de800d1961fb3b554d62593dd6
Javan <71285290+djavanw@users.noreply.github.com>
2021-06-08 23:05:43 -0600

    Switch route order back in the apiRoute.js.

--- routers/apiRoute.js
+++ routers/apiRoute.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 const { Workout } = require("../models");
 
-///Get1
+router.post("/api/workouts", async ({ body }, res) => {
+    try{
+        const daWorkout2 = await Workout.create(body);
+        res.status(201).json(daWorkout2);
+    } catch(error) {
+        res.status(400).json(error);
+    }
+});
+
+router.put("/api/workouts/:id", async ({body, params}, res) => {
+    try{
+        const daworkouts = await Workout.findByIdAndUpdate({_id: params.id},{ $push:{exercises: body}},
+            { new: true, runValidators: true }
+            )
+        //const daworkouts = await Workout.updateOne({_id: params.id},{ $push:{exercises: body}})
+        res.status(200).json(daworkouts)
+    } catch(error) {
+        console.log(error)
+    res.status(500).json("There is an error in the put route.")
+    }
+});
+
+
 router.get("/api/workouts", async (req, res) => {
     try{
         const daworkouts = await Workout.aggregate([
@@ -17,7 +39,7 @@ router.get("/api/workouts", async (req, res) => {
          res.status(500).json()
         }
 });
-///Get1
+
 
 
 // last 7 completed workouts sorted to see most recent
@@ -42,6 +64,7 @@ router.get("/api/workouts/range", async (req, res) => {
         }
 });
 
+
 // seperate route to list more than 7 exercise name
 router.get("/api/workouts/graphic", async (req, res) => {
     try{
@@ -65,28 +88,6 @@ router.get("/api/workouts/graphic", async (req, res) => {
         }
 }); 
 
-router.post("/api/workouts", async ({ body }, res) => {
-    try{
-        const daWorkout2 = await Workout.create(body);
-        res.status(201).json(daWorkout2);
-
-    } catch(error) {
-        res.status(400).json(error);
-    }
-});
-
-router.put("/api/workouts/:id", async ({body, params}, res) => {
-    try{
-        const daworkouts = await Workout.findByIdAndUpdate({_id: params.id},{ $push:{exercises: body}},
-            { new: true, runValidators: true }
-            )
-        //const daworkouts = await Workout.updateOne({_id: params.id},{ $push:{exercises: body}})
-        res.status(200).json(daworkouts)
-    } catch(error) {
-        console.log(error)
-    res.status(500).json("There is an error in the put route.")
-    }
-});
 
 /*******************DELETE*****************************************/
 router.delete("/api/workouts/:id", async ({body, params}, res) => {
